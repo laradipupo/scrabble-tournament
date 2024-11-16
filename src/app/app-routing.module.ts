6e4e8171ec9a3cd7e04613fd75adfdd8b638fd8d
Omolara Oladipupo <omolaraoladipupo11@gmail.com>
2024-11-16 20:35:47 +0000

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: '', component: HomeComponent},
+  {path: 'leaderboard', component: LeaderboardComponent},
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
